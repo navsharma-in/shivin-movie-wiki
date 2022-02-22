83536c24f59625f3175842e5abfffee7350ed355
Naveen Sharma <navsharma.in@gmail.com>
2022-02-22 16:05:45 +0530

    Change In Header
    Change

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,13 +6,14 @@ const Home = () => {
     return (
     <Container>
 
-                <h1>LET ME INTRODUCE MYSELF</h1><hr></hr>
+                <h1 className='h1'>LET ME INTRODUCE MYSELF</h1><hr></hr>
                 <p>I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️</p>
                 <p>I am fluent in classics like C++, Javascript and Python.</p>
                 <p>My field of Interest's are building new  Web Technologies and Products and also in areas related to Deep Learning and Natural Launguage Processing.
                 Whenever possible, I also apply my passion for developing products with Node.js and Modern Javascript Library and Frameworks  like React.js and Next.js</p>
 
-    </Container>
+        </Container>
+        
     )
 };
 
