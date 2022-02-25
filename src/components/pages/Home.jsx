9d788bbd0fdb56da0b7bd6f22289a869c7228fcf
Naveen Sharma <navsharma.in@gmail.com>
2022-02-25 12:14:35 +0530

    Change In Home Page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,42 +1,29 @@
-import { Box, Typography } from '@mui/material';
 import React from 'react';
 import Container from '@mui/material/Container';
-import Table from '@mui/material/Table';
+import Grid from '@mui/material/Grid';
+import logo from "./Movies/logo.svg";
+
 
 const Home = () => {
     return (
-    <div>    
-        <Container>
-
-                <h1 className='h1'>LET ME INTRODUCE MYSELF</h1><hr></hr>
-                <p>I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️</p>
-                <p>I am fluent in classics like C++, Javascript and Python.</p>
-                <p>My field of Interest's are building new  Web Technologies and Products and also in areas related to Deep Learning and Natural Launguage Processing.
-                Whenever possible, I also apply my passion for developing products with Node.js and Modern Javascript Library and Frameworks  like React.js and Next.js</p>
-                <hr></hr></Container>
-            <Container>
-
-                
-                <h1  className='h1'>This Is PPPPPPPPP </h1>
-
-                <h2 className='h1'>Hello I am The Conrainer</h2>
-               <Box> 
-                <Table>
-                <tr>
-                    <th>Name</th>
-                    <th>Class</th>
-                    <th>Mobile</th>
-                </tr>
-                <tr>
-                    <td>Naveen</td>
-                    <td>PGD-PPT</td>
-                    <td>9302550126</td>
-                </tr>
-            </Table>
-            </Box>
-            </Container>    
+        <div>
+           
+                    <Container>
+                    <Grid container spacing={2} columns={16}>
+                    <Grid item xs={10}>
+                    <h1 className='h1'>LET ME INTRODUCE MYSELF</h1>
+                        <p className='p'>I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️I am fluent in classics like C++, Javascript and Python.
+                        My field of Interest's are building new  Web Technologies and Products and also in areas related to Deep Learning and Natural Launguage Processing.
+                        Whenever possible, I also apply my passion for developing products with Node.js and Modern Javascript Library and Frameworks  like React.js and Next.js</p>
+                    </Grid>
+                    <Grid item xs={4}>
+                   <img src={logo} className="App-logo" alt="logo" /><h1>Hi...This Is Naveen</h1>
+                    </Grid>
+                    </Grid>
+                    </Container>   
+                    
             
-
+            
     </div>    
     )
 };
